22406adeb3a2f89c7805224054cd4264e47c2e06
Brian Francis <13bfrancis@gmail.com>
2018-11-06 20:46:31 -0500

    organized files and removed unessecary ones from the src folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Yahtzee from './Yahtzee';
+import Yahtzee from './Components/Yahtzee';
 
 class App extends Component {
   render() {
