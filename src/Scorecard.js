87532f2e16825a4988bd19e2b85f55731ff31780
Brian Francis <13bfrancis@gmail.com>
2018-11-06 20:36:26 -0500

    Added the Scorecard.js file starter for the Scorecard component

---
+++ src/Scorecard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Scorecard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the scorecard</h1>
+      </div>
+    );
+  }
+}
