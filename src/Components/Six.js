eb913691880d4abea9e32cf8689306632ba2e115
Brian Francis <13bfrancis@gmail.com>
2018-11-07 23:18:31 -0500

    added a react svg component for the six die

---
+++ src/Components/Six.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default ({ size = '100', fill = 'black' }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="svg3759"
+    version="1.1"
+    viewBox="0 0 100 100"
+    height={size}
+    width={size}
+    fill={fill}
+  >
+    <defs id="defs3753" />
+
+    <g transform="translate(0,-1022.5197)" id="layer1">
+      <circle r="12.5" id="ellipse3826" cx="22.5" cy="1102.5197" />
+      <circle cy="1072.5197" cx="22.5" id="circle3834" r="12.5" />
+      <circle r="12.5" id="circle3836" cx="22.5" cy="1042.5197" />
+      <circle cy="1102.5197" cx="77.5" id="circle3838" r="12.5" />
+      <circle r="12.5" id="circle3840" cx="77.5" cy="1072.5197" />
+      <circle cy="1042.5197" cx="77.5" id="circle3842" r="12.5" />
+    </g>
+  </svg>
+);
